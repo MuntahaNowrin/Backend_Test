b2042ca1e69aab326835da6dc717279307310f0f
MuntahaNowrin <muntahanowrin11@gmail.com>
2021-10-15 12:58:20 +0200

    Added a Test case

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -1,22 +1,30 @@
 package com.test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","F:\\Jenkins\\workspace\\chromedriver.exe"); // <-- Change this path
-        WebDriver driver = new ChromeDriver();
-        String baseUrl = "https://experitest.com/free-trial/";
-        String expectedTitle = "Free trial";
-        String actualTitle = "";
-        driver.get(baseUrl);
-        actualTitle = driver.getTitle();
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("TEST PASSED!");
-        } else {
-            System.out.println("TEST FAILED");
-        }
-        driver.close();
+    	String projectpath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectpath + "/lib/chromedriver/chromedriver");
+		driver = new ChromeDriver();
+
+		driver.get("http://localhost:8085/java-tomcat-maven-example/");
+
+		String bodyText = driver.findElement(By.tagName("h2")).getText();
+		System.out.println(bodyText);
+
+		if (bodyText.contains("Hello World!")) {
+
+			System.out.println("TEST PASSED!");
+
+		} else {
+
+			System.out.println("TEST FAILED");
+		}
+
+		driver.close();
+
     }
 }
